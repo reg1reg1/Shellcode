312e6bb356d8782a6356c89bc4417e4e23684253
Yuvraj Singh <yuvraj.singh19@gmail.com>
2020-06-12 00:20:37 -0700

    Restructuring Folders for clarity

--- Linux/x86/assembly-basics/HelloWorld.asm
+++ Linux/x86/assembly-basics/HelloWorld.asm
@@ -1,7 +1,9 @@
 ;Hello World Written for NASM assembler
 ;Notice the differences between the GAS and NASM syntax
 
-.global _start
+;ssize_t write(int fd, const void *buf, size_t count);
+
+global _start
 
 
 
@@ -9,8 +11,16 @@ section .text
 
 
 _start:
-	mov eax, 
+	mov eax, 0x04
+	mov ebx, 0x01
+	mov ecx, message 
+	mov edx, 0x0b
+	int 0x80
 
+	xor eax,eax
+	add al,1
+	mov ebx,5
+	int 0x80
 
 section .data
 	message: db "Hello World"
